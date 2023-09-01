a0bc13253e0b580f2cf677d36d2ab107cc186c21
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-09-01 11:28:26 +0800

    :construction_worker: remove sudo in create-package.sh

--- scripts/create-package.sh
+++ scripts/create-package.sh
@@ -17,20 +17,20 @@ rootfs=$workdir/rootfs
 mkdir -p $rootfs
 
 # Extract ramdisk and preserve ownership of files
-(cd $rootfs ; cat $ramdisk | gzip -d | sudo cpio -i)
+(cd $rootfs ; cat $ramdisk | gzip -d | cpio -i)
 
 mkdir $workdir/system
-sudo mount -o loop,ro $system $workdir/system
-sudo cp -ar $workdir/system/* $rootfs/system
-sudo umount $workdir/system
+mount -o loop,ro $system $workdir/system
+cp -ar $workdir/system/* $rootfs/system
+umount $workdir/system
 
 gcc -o $workdir/uidmapshift external/nsexec/uidmapshift.c
-sudo $workdir/uidmapshift -b $rootfs 0 100000 65536
+$workdir/uidmapshift -b $rootfs 0 100000 65536
 
 # FIXME
-sudo chmod +x $rootfs/anbox-init.sh
+chmod +x $rootfs/anbox-init.sh
 
-sudo mksquashfs $rootfs $image -comp xz -no-xattrs
-sudo chown $USER:$USER $image
+mksquashfs $rootfs $image -comp xz -no-xattrs
+chown $USER:$USER $image
 
-sudo rm -rf $workdir
+rm -rf $workdir
